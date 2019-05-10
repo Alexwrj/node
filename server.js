907ca74d0d7f25c23345e046bf8abc96af593692
Alexwrj <32782446+polygonator@users.noreply.github.com>
2019-05-10 16:00:10 +0300

    1

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response){
-	console.log("Received!");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello, World!");
-	response.end();
-}).listen(8888);
+function start(route){
+	http.createServer(function(request, response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for" + pathname + " received.");
 
-console.log("Server started!");
+		route(pathname);
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello, World!");
+		response.end();
+	}).listen(8888);
+
+	console.log("Server started!");
+}
+
+exports.start = start;
